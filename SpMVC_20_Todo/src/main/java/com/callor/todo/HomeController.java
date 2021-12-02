a29c20cf87fe73a6c076f468c58ff079f6ad1aa5
callor <callor@callor.com>
2021-12-02 11:52:38 +0900

    FileUpload

---
+++ SpMVC_20_Todo/src/main/java/com/callor/todo/HomeController.java
@@ -0,0 +1,15 @@
+package com.callor.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
+}
---
+++ SpMVC_20_Todo/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:set var="rootPath" value="${pageContext.request.contextPath}" />    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>나의 홈페이지</h1>
+	<a href="${rootPath}/file">파일업로드</a>
+</body>
+</html>
+
